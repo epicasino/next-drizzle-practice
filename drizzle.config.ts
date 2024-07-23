af796b24ffd27f4a2e0c038b0650ab86bfb2546c
Ray Badua <rayjonathanbadua@gmail.com>
2024-07-23 01:55:04 -0700

    user schema in schema.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,8 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './db/schema.ts',
-  out: './drizzle',
+  schema: './src/db/schema.ts',
+  out: './src/drizzle',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.NEON_DATABASE_URL!,
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,14 @@
-import { integer, text, boolean, pgTable } from 'drizzle-orm/pg-core';
+import { text, boolean, uuid, pgTable } from 'drizzle-orm/pg-core';
+
+export const user = pgTable('users', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  username: text('name'),
+  password: text('password'),
+});
 
 export const todo = pgTable('todo', {
-  id: integer('id').primaryKey(),
+  id: uuid('id').defaultRandom().primaryKey(),
   text: text('text').notNull(),
   done: boolean('done').default(false).notNull(),
+  userId: uuid('user_id').references(() => user.id),
 });
